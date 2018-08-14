73080eac6b7b7eb3ec1e9a9518087c4ef0c677c7
Piyanan Kamlangmark <duck_comen@hotmail.com>
2018-08-14 18:37:36 +0700

    correct mesage

--- webhooks.php
+++ webhooks.php
@@ -10,37 +10,39 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 // Loop through each event
-foreach ($events['events'] as $event) {
+	foreach ($events['events'] as $event) {
 // Reply only when message sent is in 'text' format
-if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 // Get text sent
-$text = $event['source']['userId'];
+			$text = $event['source']['userId'];
 // Get replyToken
-$replyToken = $event['replyToken'];
+			$replyToken = $event['replyToken'];
 // Build message to reply back
-$messages = [
-'type' => 'text',
-'text' => $text
-];
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 // Make a POST Request to Messaging API to reply to sender
-$url = 'https://api.line.me/v2/bot/message/reply';
-$data = [
-'replyToken' => $replyToken,
-'messages' => [$messages],
-];
-$post = json_encode($data);
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-echo $result . "\r\n";
-}
-}
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
+		}
+	}
 }
 echo "Initial Version 0.1";
-echo "credit : https://medium.com/@sorajbuasri/%E0%B8%A1%E0%B8%B2%E0%B8%A5%E0%B8%AD%E0%B8%87%E0%B9%80%E0%B8%A5%E0%B9%88%E0%B8%99-line-bot-%E0%B9%82%E0%B8%94%E0%B8%A2%E0%B9%83%E0%B8%8A%E0%B9%89-message-api-line-%E0%B8%81%E0%B8%B1%E0%B8%99%E0%B9%80%E0%B8%96%E0%B8%AD%E0%B8%B0-php-7ad80fe8328a";
+$link_address = 'https://medium.com/@sorajbuasri/%E0%B8%A1%E0%B8%B2%E0%B8%A5%E0%B8%AD%E0%B8%87%E0%B9%80%E0%B8%A5%E0%B9%88%E0%B8%99-line-bot-%E0%B9%82%E0%B8%94%E0%B8%A2%E0%B9%83%E0%B8%8A%E0%B9%89-message-api-line-%E0%B8%81%E0%B8%B1%E0%B8%99%E0%B9%80%E0%B8%96%E0%B8%AD%E0%B8%B0-php-7ad80fe8328a';
+echo '<a href="$link_address">Link</a>';
+echo "credit : ";
