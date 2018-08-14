8568adacffaa514e1d98ac641ac70bfe58732ac9
Piyanan Kamlangmark <duck_comen@hotmail.com>
2018-08-14 18:40:27 +0700

    credit link

--- webhooks.php
+++ webhooks.php
@@ -44,5 +44,5 @@ if (!is_null($events['events'])) {
 }
 echo "Initial Version 0.1";
 $link_address = 'https://medium.com/@sorajbuasri/%E0%B8%A1%E0%B8%B2%E0%B8%A5%E0%B8%AD%E0%B8%87%E0%B9%80%E0%B8%A5%E0%B9%88%E0%B8%99-line-bot-%E0%B9%82%E0%B8%94%E0%B8%A2%E0%B9%83%E0%B8%8A%E0%B9%89-message-api-line-%E0%B8%81%E0%B8%B1%E0%B8%99%E0%B9%80%E0%B8%96%E0%B8%AD%E0%B8%B0-php-7ad80fe8328a';
-echo '<a href="$link_address">Link</a>';
 echo "credit : ";
+echo "<a href='$link_address'>Link</a>";
